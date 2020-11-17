9bd502494beb0651dbdcabb05ec3263ef8416e44
Juan Garcia <juagarca@gmail.com>
2020-11-17 12:05:43 +0000

    dogs routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:show, :index]
+  resources :users, only: [:show, :index] do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :dogs, only: :new
+  end
 
+  resources :dogs, only: [:show, :index]
 end
