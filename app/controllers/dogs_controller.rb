2d046ba99edee42034021ee0bd8d3bed02c4a335
Juan Garcia <juagarca@gmail.com>
2020-11-17 16:47:41 +0000

    fixedlinksandhovers2

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,17 +1,16 @@
 class DogsController < ApplicationController
-before_action :set_dog, only: [:show]
+  before_action :set_dog, only: [:show]
 
-  def show    
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
   end
 
   private
 
   def set_dog
     @dog = Dog.find(params[:id])
-  end
-
-  def index
-    @dogs = Dog.all
-    @search = params["postcode"]
   end
 end
