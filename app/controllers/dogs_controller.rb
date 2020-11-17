b416ca82a0b6127667b7a1556f9908daf01d71ba
John Tweeddale <j.tweeddale@outlook.com>
2020-11-17 12:37:24 +0000

    add show action to dogs controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,13 @@
 class DogsController < ApplicationController
+before_action :set_dog, only: [:show]
+
+  def show    
+  end
+
+  private
+
+  def set_dog
+    @dog = Dog.find(params[:id])
+  end
+
 end
