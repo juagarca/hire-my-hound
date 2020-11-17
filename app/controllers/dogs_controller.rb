64025c01f4107f4f769f84acb6792aa9289836c6
Bethany Armstrong <bethanyarmstrong1@live.co.uk>
2020-11-17 17:38:12 +0000

    profile

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,20 @@
 class DogsController < ApplicationController
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+    @dog.user = current_user
+    @dog.save
+    redirect_to user_path(current_user)
+  end
+
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
 end
