a8dc574516ae1608ed7732f48d0343c8ba7f7ab3
John Tweeddale <j.tweeddale@outlook.com>
2020-11-18 13:08:22 +0000

    fix user update form and add user logo to navbar

--- db/schema.rb
+++ db/schema.rb
@@ -56,13 +56,13 @@ ActiveRecord::Schema.define(version: 2020_11_18_115823) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.float "latitude"
-    t.float "longitude"
     t.string "first_name"
     t.string "last_name"
     t.string "address"
     t.text "bio"
     t.date "date_of_birth"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
