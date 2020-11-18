127464b3665cd9547295aa20036fa48ce9dd3cba
John Tweeddale <j.tweeddale@outlook.com>
2020-11-18 10:54:59 +0000

    add user edit route, action, and set_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,20 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit]
 
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
+  def edit
+  end
 
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
 end
