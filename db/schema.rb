8d0723aac90379d7fe06478ae3c0391c6d9be821
Juan Garcia <juagarca@gmail.com>
2020-11-19 17:40:18 +0000

    small fixes and navbar to go gray on scroll

--- db/schema.rb
+++ db/schema.rb
@@ -71,13 +71,13 @@ ActiveRecord::Schema.define(version: 2020_11_19_112649) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.string "first_name"
     t.string "last_name"
     t.string "address"
     t.text "bio"
     t.date "date_of_birth"
-    t.float "latitude"
-    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
