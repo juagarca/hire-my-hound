a44e32b1ac99eeae9225bb576573c84fe56183b9
Juan Garcia <juagarca@gmail.com>
2020-11-20 07:58:16 +0000

    fixed search, location and created payslips menu

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -3,35 +3,31 @@ class DogsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @dogs = []
-    # Finding users with dogs
-    users = []
-    @dogs.each do |dog|
-      users << dog.user
-    end
-
-    # Finding users within 10 miles
+    # If search find users within 10 miles
     if params[:query].present?
+      @dogs = []
       # @dogs = Dog.joins(:user).where(users: { address: params[:query] })
       users = User.near(params[:query], 10)
       # Adding their dogs to @dogs
       users.each do |user|
         user.dogs.each { |dog| @dogs << dog }
       end
+      # Finding users with dogs
+      users = users.reject { |user| user.dogs == [] }
+
+      @markers = users.map do |u|
+        {
+          lat: u.latitude,
+          lng: u.longitude,
+          infoWindow: render_to_string(partial: "info_window", locals: { user: u })
+        }
+      end
     else
       # if not search then showing all dogs
       @dogs = Dog.all
     end
 
-    users = users.reject { |user| user.latitude.nil? || user.longitude.nil? }
 
-    @markers = users.map do |u|
-      {
-        lat: u.latitude,
-        lng: u.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { user: u })
-      }
-    end
   end
 
   def new
